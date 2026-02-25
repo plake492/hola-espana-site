2bd0bc1a8ba4a13d0a6af4e1658caf6a219b57f0
Patrick Lake <patricklake@Patricks-MacBook-Pro-3.local>
2026-02-25 13:56:35 -0500

    Remove broken imports

--- components/preview/index.tsx
+++ components/preview/index.tsx
@@ -2,8 +2,6 @@ import Hero from './Hero';
 import WeKnowSpain from './WeKnowSpain';
 import WhatWeDo from './WhatWeDo';
 import WhyBookUs from '../home/WhyChooseUs';
-import HowItWorks from '../home/HowItWorks';
-import OurTeam from '../home/OurTeam';
 import Reviews from '../home/Reviews';
 import Blogs from '../home/Blogs';
 import CTA from './CTA';
@@ -11,4 +9,4 @@ import HeroCta from '../home/HeroCta';
 import Services from './Services';
 import About from './About';
 
-export { Hero, WeKnowSpain, WhatWeDo, WhyBookUs, HowItWorks, OurTeam, Reviews, Blogs, CTA, HeroCta, Services, About };
+export { Hero, WeKnowSpain, WhatWeDo, WhyBookUs, Reviews, Blogs, CTA, HeroCta, Services, About };
